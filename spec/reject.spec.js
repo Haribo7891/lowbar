ff17e46e78c0ddc1b6369410f693340a7527df1f
Haribo7891 <harry.crank@me.com>
2017-11-07 16:41:56 +0000

    #reject typo in tests

--- spec/reject.spec.js
+++ spec/reject.spec.js
@@ -5,17 +5,17 @@ describe('#reject', () => {
   it('Exists', () => {
     expect(_.reject).to.be.a('function');
   });
-  it('Returns items from strings that pass the predicate', () => {
+  it('Returns items from strings that don\'t pass the predicate', () => {
     const str = 'YummyHotDog';
     const predicate = (letter => letter === letter.toLowerCase());
     expect(_.reject(str, predicate)).to.eql(['Y', 'H', 'D']);
   });
-  it('Returns items from arrays that pass the predicate', () => {
+  it('Returns items from arrays that don\'t pass the predicate', () => {
     const arr = [1,2,3,4,5];
     const predicate = (num => num < 3);
     expect(_.reject(arr, predicate)).to.eql([3,4,5]);
   });
-  it('Returns items from objects that pass the predicate', () => {
+  it('Returns items from objects that don\'t pass the predicate', () => {
     const obj = {a:6, b:3, c:4};
     const predicate = (num => num % 2 === 0);
     expect(_.reject(obj, predicate)).to.eql([3]);
