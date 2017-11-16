63e75aaa558a45ef97637b96232ba782332278a3
Haribo7891 <harry.crank@me.com>
2017-11-16 15:00:56 +0000

    #some test pass

--- collectionFunctions/some.js
+++ collectionFunctions/some.js
@@ -1,6 +1,18 @@
 const _ = {};
+const { identity } = require('../utilityFunctions/identity');
 
-_.some = function () {
+_.some = function (list, predicate) {
+  predicate = predicate || identity;
+  if(list && list.length) {
+    for (let i = 0; i < list.length; i++) {
+      if(predicate(list[i], i, list)) return true;
+    }
+  } else if (typeof list === 'object') {
+    for (let key in list) {
+      if (predicate(list[key], key, list)) return true;
+    }
+  }
+  return false;
 };
 
 if (typeof module !== 'undefined') {
