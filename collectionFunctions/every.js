3b76af4f74050d8a7b14af70715928da04a0a3e7
Haribo7891 <harry.crank@me.com>
2017-11-16 14:46:33 +0000

    #every test pass

--- collectionFunctions/every.js
+++ collectionFunctions/every.js
@@ -1,6 +1,18 @@
 const _ = {};
+const { identity } = require('../utilityFunctions/identity');
 
-_.every = function () {
+_.every = function (list, predicate) {
+  predicate = predicate || identity;
+  if(list && list.length) {
+    for (let i = 0; i < list.length; i++) {
+      if(!predicate(list[i], i, list)) return false;
+    }
+  } else if (typeof list === 'object') {
+    for (let key in list) {
+      if (!predicate(list[key], key, list)) return false;
+    }
+  }
+  return true;
 };
 
 if (typeof module !== 'undefined') {
