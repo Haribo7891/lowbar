a67254981d355da866efba0b9b425d0b16e21621
Haribo7891 <harry.crank@me.com>
2017-11-17 10:19:22 +0000

    #reduce test pass

--- collectionFunctions/reduce.js
+++ collectionFunctions/reduce.js
@@ -1,6 +1,19 @@
 const _ = {};
+const { identity } = require('../utilityFunctions/identity');
+const { each } = require('./each');
 
-_.reduce = function () {
+_.reduce = function (list, iteratee, memo) {
+  iteratee = iteratee || identity;
+  let noMemo = memo === undefined;
+  each(list, (item, index, list) => {
+    if(noMemo) {
+      memo = item;
+      noMemo = false;
+    } else {
+      memo = iteratee(memo, item, index, list);
+    }
+  });
+  return memo;
 };
 
 if (typeof module !== 'undefined') {
--- spec/reduce.spec.js
+++ spec/reduce.spec.js
@@ -5,4 +5,33 @@ describe('#reduce', () => {
   it('Exists', () => {
     expect(_.reduce).to.be.a('function');
   });
+  it('Returns a single result from an array of values when passed an accumulator', () => {
+    const input = [1,2,3,4,5];
+    const sum = (acc, num) => acc + num;
+    expect(_.reduce(input, sum, 0)).to.equal(15);
+  });
+  it('Returns a single result from an array of values when not passed with an accumulator', () => {
+    const input = [5,4,3,2,1];
+    const sum = (acc, num) => acc + num;
+    expect(_.reduce(input, sum)).to.equal(15);
+  });
+  it('Returns a single result from an object of values when passed an accumulator', () => {
+    const input = {a:1, b:2, c:3, d:4, e:5};
+    const sum = (acc, num) => acc + num;
+    expect(_.reduce(input, sum, 0)).to.equal(15);
+  });
+  it('Returns a single result from an object of values when not passed with an accumulator', () => {
+    const input = {a:1, b:2, c:3, d:4, e:5};
+    const sum = (acc, num) => acc + num;
+    expect(_.reduce(input, sum)).to.equal(15);
+  });
+  it('Returns a count object', () => {
+    const input = ['h', 'a', 'r', 'r', 'y'];
+    const createCount = (acc, item) => {
+      acc.hasOwnProperty(item) ? acc[item] += 1 : acc[item] = 1;
+      return acc;
+    };
+    const expected = {h:1, a:1, r:2, y:1};
+    expect(_.reduce(input, createCount, {})).to.eql(expected);
+  });
 });
