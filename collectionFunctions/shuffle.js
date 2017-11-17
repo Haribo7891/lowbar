7204a571dad40da783692335adbf70001ebff7c1
Haribo7891 <harry.crank@me.com>
2017-11-17 11:45:39 +0000

    #shuffle test pass

--- collectionFunctions/shuffle.js
+++ collectionFunctions/shuffle.js
@@ -1,6 +1,19 @@
 const _ = {};
+const { map } = require('./map');
 
-_.shuffle = function () {
+_.shuffle = function (list) {
+  const arr = map(list);
+  let index = arr.length;
+  let randomItem;
+  let temp;
+  while(index) {
+    randomItem = Math.floor(Math.random() * index);
+    index--;
+    temp = arr[index];
+    arr[index] = arr[randomItem];
+    arr[randomItem] = temp;
+  }
+  return arr;
 };
 
 if (typeof module !== 'undefined') {
--- spec/shuffle.spec.js
+++ spec/shuffle.spec.js
@@ -5,4 +5,20 @@ describe('#shuffle', () => {
   it('Exists', () => {
     expect(_.shuffle).to.be.a('function');
   });
+  it('Returns a shuffled copy of the input array', () => {
+    const arr = [1,2,3,4,5];
+    expect(_.shuffle(arr).length).to.equal(arr.length);
+  });
+  it('Returns the shuffled copy of the input array unmutated', () => {
+    const arr = [1,2,3,4,5];
+    expect(_.shuffle(arr)).to.not.eql(arr);
+  });
+  it('Returns a shuffled copy of the input string', () => {
+    const str = 'abcde';
+    expect(_.shuffle(str).length).to.equal(str.length);
+  });
+  it('Returns the shuffled copy of the input string unmutated', () => {
+    const str = 'abcde';
+    expect(_.shuffle(str)).to.not.eql(str);
+  });
 });
