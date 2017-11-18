a27a9aa973062108dcdb8ff290a706ca734832ee
Haribo7891 <harry.crank@me.com>
2017-11-18 12:59:18 +0000

    #zip test pass

--- arrayFunctions/zip.js
+++ arrayFunctions/zip.js
@@ -1,6 +1,14 @@
 const _ = {};
+const { pluck } = require('../collectionFunctions/pluck');
 
-_.zip = function () {
+_.zip = function (...list) {
+  const longestArray = Math.max(...list.map((arr) => arr.length));
+  const maxLength = Math.max(list.length, longestArray);
+  const newArray = Array(maxLength);
+  for (let i = 0; i < maxLength; i++) {
+    newArray[i] = pluck(list, i);
+  }
+  return newArray;
 };
 
 if (typeof module !== 'undefined') {
--- spec/zip.spec.js
+++ spec/zip.spec.js
@@ -5,4 +5,16 @@ describe('#zip', () => {
   it('Exists', () => {
     expect(_.zip).to.be.a('function');
   });
+  it('Returns a merged array when passed arrays of equal length', () => {
+    const arr1 = [ 1, 2, 3 ];
+    const arr2 = [ 'a', 'b', 'c' ];
+    const expected = [[ 1, 'a' ], [ 2, 'b' ], [ 3, 'c' ]];
+    expect(_.zip(arr1, arr2)).to.eql(expected);
+  });
+  it('Returns a merged array when passed arrays of unequal length', () => {
+    const arr1 = [ 1, 2, 3, 4 ];
+    const arr2 = [ 'a', 'b', 'c' ];
+    const expected = [[ 1, 'a' ], [ 2, 'b' ], [ 3, 'c' ], [ 4, undefined ]];
+    expect(_.zip(arr1, arr2)).to.eql(expected);
+  });
 });
