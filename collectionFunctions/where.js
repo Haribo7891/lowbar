0dad4b8e67c336db3649b53d020c05f6a30c5897
Haribo7891 <harry.crank@me.com>
2017-11-18 13:38:40 +0000

    #where test pass

--- collectionFunctions/where.js
+++ collectionFunctions/where.js
@@ -1,6 +1,11 @@
 const _ = {};
+const { filter } = require('./filter');
+const { every } = require('./every');
 
-_.where = function () {
+_.where = function (list, properties) {
+  return filter(list, (item) => 
+    every(properties, (value, key) => item[key] === value)
+  );
 };
 
 if (typeof module !== 'undefined') {
--- spec/where.spec.js
+++ spec/where.spec.js
@@ -5,4 +5,36 @@ describe('#where', () => {
   it('Exists', () => {
     expect(_.where).to.be.a('function');
   });
+  it('Returns an array of all the values that match the key/value pairs passed in', () => {
+    const input = [
+      { a: 1, e: 5 },
+      { a: 1, c: 3, e: 5 },
+      { a: 1, b: 2, e: 5 },
+      { a: 1, d: 4, e: 5 },
+      { a: 1, b: 2, d: 4, e: 5 },
+      { a: 1, b: 2, c: 3, d: 4, e: 5 }
+    ];
+    const expected = [
+      { a: 1, b: 2, d: 4, e: 5 },
+      { a: 1, b: 2, c: 3, d: 4, e: 5 }
+    ];
+    expect(_.where(input, { a: 1, b: 2, d: 4, e: 5 })).to.eql(expected);
+  });
+  it('Returns the original array if given no properties', () => {
+    const input = [
+      { a: 1 },
+      { a: 1, b: 2 },
+      { a: 1, b: 2, c: 3 },
+      { a: 1, b: 2, c: 3, d: 4 },
+      { a: 1, b: 2, c: 3, d: 4, e: 5 }
+    ];
+    const expected = [
+      { a: 1 },
+      { a: 1, b: 2 },
+      { a: 1, b: 2, c: 3 },
+      { a: 1, b: 2, c: 3, d: 4 },
+      { a: 1, b: 2, c: 3, d: 4, e: 5 }
+    ];
+    expect(_.where(input)).to.eql(expected);
+  });
 });
