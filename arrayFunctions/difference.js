d12f13c0786994031b79d977816e4da61364b618
Haribo7891 <harry.crank@me.com>
2017-11-18 13:58:11 +0000

    #difference test pass

--- arrayFunctions/difference.js
+++ arrayFunctions/difference.js
@@ -1,6 +1,11 @@
 const _ = {};
+const { filter } = require('../collectionFunctions/filter');
+const { every } = require('../collectionFunctions/every');
+const { contains } = require('../collectionFunctions/contains');
 
-_.difference = function () {
+_.difference = function (list, ...otherLists) {
+  return filter(list, (item) => 
+    every(otherLists, (otherItem) => !contains(otherItem, item)));
 };
 
 if (typeof module !== 'undefined') {
--- spec/difference.spec.js
+++ spec/difference.spec.js
@@ -5,4 +5,16 @@ describe('#difference', () => {
   it('Exists', () => {
     expect(_.difference).to.be.a('function');
   });
+  it('Returns an array of values not found in the other array passed in', () => {
+    const input = [ 1, 2, 3, 4, 5 ];
+    const expected = [ 2, 4 ];
+    expect(_.difference(input, [ 1, 3, 5 ])).to.eql(expected);
+  });
+  it('Returns an array of values not found in the other arrays passed in', () => {
+    const input = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+    const otherInput1 = [ 1, 6, 9 ];
+    const otherInput2 = [ 2, 5, 8 ];
+    const expected = [ 3, 4, 7 ];
+    expect(_.difference(input, otherInput1, otherInput2)).to.eql(expected);
+  });
 });
